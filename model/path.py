a2ee596afd4c2a025972973c037ba1dd4da1cfb5
Markuss2b <s22blummark@venta.lv>
2025-03-24 16:31:44 +0200

    path logic mostly finished

--- model/path.py
+++ model/path.py
@@ -1,4 +1,5 @@
 # TODO might need to change indexes
+import os
 
 class Path:
     def __init__(self, name, max_x, max_y):
@@ -20,55 +21,109 @@ class Path:
                 
                 
     def draw_path(self):
-        for i in self.sequence:
+        print("==================================")
+        for i in self.path_tiles:
             for j in i:
                 print(j, end=" ")
-            print()                
+            print()
+        print("==================================")                
     
     
     def add_next_step(self, x, y):
         last_move = self.sequence[-1]
         try:
-            if abs(last_move.x - x) == 0 and abs(last_move.y - y) == 1 or abs(last_move.x - x) == 1 and abs(last_move.y - y) == 0 and x < self.max_x and x >= 0 and y < self.max_y and y >= 0:
-                self.sequence.append(Location(x, y))
-                self.path_tiles[y][x] = len(self.sequence)
+            # If statements check if next step is connected with the last move. Also it checks if its out of bounds
+            if abs(last_move.x - x) == 0 and abs(last_move.y - y) == 1 or abs(last_move.x - x) == 1 and abs(last_move.y - y) == 0:
+                if x <= self.max_x and x >= 0 and y <= self.max_y and y >= 0:
+                    self.sequence.append(Location(x, y))
+                    self.path_tiles[y][x] = len(self.sequence)
+                else:
+                    raise Exception
+            else:
+                raise Exception
+            if self.start_location == None:
+                raise Exception
         except Exception:
             # TODO     
             print("Cant connect moves")
             
             
     def remove_step(self):
-        self.path_tiles[self.sequence.y][self.sequence.x] = 0
-        self.sequence.remove(self.sequence[-1])
+        if len(self.sequence) > 1:
+            self.path_tiles[self.sequence[-1].y][self.sequence[-1].x] = 0
+            self.sequence.remove(self.sequence[-1])
+        else:
+            print("No steps to remove")
     
     
     def set_start(self, x, y):
-        self.start_location = Location(x, y)
-        self.sequence[0] = self.start_location
-        self.path_tiles[y][x] = 1
+        if x <= self.max_x and x >= 0 and y <= self.max_y and y >= 0:
+            # Only remove previous value when start_location has not been set
+            if self.start_location != None:
+                self.path_tiles[self.start_location.y][self.start_location.x] = 0
+                
+            self.start_location = Location(x, y)
+            self.sequence[0] = self.start_location
+            self.path_tiles[y][x] = 1
     def get_start(self):
         return self.start_location
     
+    #TODO add_step
     def set_end(self, x, y):
-        self.end_location = Location(x, y)
-        self.sequence.append(Location(x, y))
-        self.path_tiles[y][x] = len(self.sequence)
+        if x <= self.max_x and x >= 0 and y <= self.max_y and y >= 0:
+            # Only remove previous value when start_location has not been set
+            if self.end_location != None:
+                self.path_tiles[self.end_location.y][self.end_location.x] = 0
+            
+            self.end_location = Location(x, y)
+            self.sequence.append(Location(x, y))
+            self.path_tiles[y][x] = len(self.sequence)
     def get_end(self):
         return self.end_location
     
     
     def save_path(self, map_name):
-        f = open(f'./all_maps/{map_name}/{self.name}.txt', "w")
-        for i in len(self.path_tiles):
-            for j in len(self.path_tiles[i]):
+        os.mkdir(f'./all_maps/{map_name}/paths')
+        f = open(f'./all_maps/{map_name}/paths/{self.name}.txt', "w")
+        for i in range(len(self.path_tiles)):
+            for j in range(len(self.path_tiles[i])):
                 f.write(f'{self.path_tiles[i][j]} ')
             f.write("\n")
         f.close()
 
     
-    def recreate_path_from_file(self):
-        pass
-    
+    def recreate_path_from_file(self, map_name):
+        with open(f'./all_maps/{map_name}/paths/{self.name}.txt', "r") as path_file:
+            # Predefined first value of sequence that needs to be removed when recreating the path from the file
+            self.sequence.remove(None)
+            
+            txt_file_list = [line.split() for line in path_file]
+            self.max_x = len(txt_file_list[0])
+            self.max_y = len(txt_file_list)
+            
+            dict_sequence = {} 
+            
+            for y in range(self.max_x):
+                self.path_tiles.append([])
+                
+                for x in range(self.max_y):
+                    value = int(txt_file_list[y][x])
+                    
+                    self.path_tiles[y].append(value)
+                    if value == 1:
+                        self.start_location = Location(x, y)
+                    if value > 0:
+                        dict_sequence[value] = Location(x, y)
+                 
+            # Sorting sequence in rising order
+            sorted_dict_sequence = dict(sorted(dict_sequence.items()))
+            for dict_key in sorted_dict_sequence:
+                self.sequence.append(sorted_dict_sequence.get(dict_key))
+            
+            # Prints it
+            for i in self.sequence:
+                print(f'x={i.x}, y={i.y}')
+
     
     def validate_path(self):
         pass
